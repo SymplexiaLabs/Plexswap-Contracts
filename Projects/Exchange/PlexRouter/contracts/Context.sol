745a648f5697cde2b2a72fbde8bd3304a262cd56
Symplexia Labs <suporte@symplexia.com.br>
2022-06-17 19:45:51 -0300

    New Router

--- Projects/Exchange/PlexRouter/contracts/Context.sol
+++ Projects/Exchange/PlexRouter/contracts/Context.sol
@@ -18,7 +18,4 @@ abstract contract Context {
         return msg.sender;
     }
 
-    function _msgData() internal view virtual returns (bytes calldata) {
-        return msg.data;
-    }
 }
--- Projects/Exchange/PlexRouter/contracts/PlexswapLibrary.sol
+++ Projects/Exchange/PlexRouter/contracts/PlexswapLibrary.sol
@@ -29,7 +29,7 @@ library PlexswapLibrary {
                         hex"ff",
                         factory,
                         keccak256(abi.encodePacked(token0, token1)),
-                        hex"a5934690703a592a07e841ca29d5e5c79b5e22ed4749057bb216dc31100be1c0" // init code hash
+                        hex"0ac3000920e9b3c229fdd8305aa7b28c16e5d16c66e4983a4e2ff27aff8e4b53" // init code hash
                     )
                 )
             )
