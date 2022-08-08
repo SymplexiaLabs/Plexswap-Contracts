a63f55ee30cf65e7784cdfe6a33cd6d6f3974a69
Symplexia Labs <suporte@symplexia.com.br>
2022-08-08 14:21:25 -0300

    WayaPool & Minor Adjust

---
+++ Projects/Farms-Barns/WayaPool/config.ts
@@ -0,0 +1,31 @@
+export default {
+  StakedToken: {
+    // WAYA
+    mainnet: "0x0581c0dae41F19Fb4602E7ba0A803B7AE50f02E8",
+    testnet: "0x156644aF8F57BD264cedeF94e9c410DA2BB19B26",
+  },
+  RewardToken: {
+    mainnet: "0x0000000000000000000000000000000000000000",
+    testnet: "0x0000000000000000000000000000000000000000",
+  },
+  RewardPerBlock: {
+    mainnet: "0.000",
+    testnet: "0.000",
+  },
+  StartBlock: {
+    mainnet: "0",
+    testnet: "0",
+  },
+  EndBlock: {
+    mainnet: "0",
+    testnet: "0",
+  },
+  PoolLimitPerUser: {
+    mainnet: "0",
+    testnet: "0",
+  },
+  Admin: {
+    mainnet: "0x0000000000000000000000000000000000000000",
+    testnet: "0x0000000000000000000000000000000000000000",
+  },
+};
